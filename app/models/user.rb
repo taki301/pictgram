7fed87dc6ae98f1639008a7056e5ed595eff62c2
taki301 <taki_tx@hotmail.com>
2020-04-13 12:47:18 +0000

    second commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: {maximum: 15}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}
+
+  
+  has_secure_password
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i
+  validates :password, length: { in: 8..32 }, format: {with: VALID_PASSWORD_REGEX}
 end
