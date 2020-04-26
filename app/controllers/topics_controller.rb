fc95e9716c97cf3f027087d64e3e7abb5191ec83
taki301 <taki_tx@hotmail.com>
2020-04-26 13:06:20 +0000

    sixth commit

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,7 +2,7 @@ class TopicsController < ApplicationController
   # before_action :login_check, {only: [:new]}
  
   def index
-    @topics = Topic.all
+    @topics = Topic.all.includes(:favorite_users)
   end
   
   def new
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,6 @@ class User < ApplicationRecord
   validates :password, length: { in: 8..32 }, format: {with: VALID_PASSWORD_REGEX}
   
   has_many :topics
+  has_many :favorites
+  has_many :favorite_topics, through: :favorites, source: 'topic'
 end
