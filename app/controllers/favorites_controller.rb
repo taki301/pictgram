77c344b6ef6c45ba0c6b801fffa3a886dd7311fd
taki301 <taki_tx@hotmail.com>
2020-05-04 00:58:07 +0000

    eigh commit

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -14,4 +14,10 @@ class FavoritesController < ApplicationController
       redirect_to topics_path, danger: 'お気に入り登録に失敗しました'
     end
   end
+  
+  def destroy
+    @favorite = Favorite.find_by(user_id: current_user.id, topic_id: params[:topic_id])
+    @favorite.destroy
+    redirect_to topics_path, success: 'お気に入りを取り消しました'
+  end
 end
